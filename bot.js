3214fa32c6a52159e8a2554e7bbbb399be793be3
abdalrhmanmamas <52424062+abdalrhmanmamas@users.noreply.github.com>
2019-09-14 11:01:35 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,62 @@
+
+
+const moment = require('moment');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', function(){
+    var ms = 10000 ;
+    var setGame = ['AhMaD','AhMaD','AhMaD','AhMaD','AhMaD'];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+    }, ms);
+  console.log('---------------');
+  console.log('Bot Is Online')
+  console.log('---------------')
+});
+
+
+var prefix = "$";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`London Server`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
